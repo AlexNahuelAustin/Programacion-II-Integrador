d138f11680eb68d642c03894dfc4190b83dfd1bc
Cristian690 <cga_1985@hotmail.com>
2025-11-18 19:24:43 -0300

    Agregando capa DAO

---
+++ HistorialClinico/src/dao/GenericDAO.java
@@ -0,0 +1,20 @@
+package dao;
+
+import java.util.List;
+/**
+ -Grupo: 50
+ -Alumnos: 
+ * Alex Nahuel Austin-Comision 17
+ * Cristian Gabriel Aguirre-Comision 6
+ * Cain Cabrera Bertilazzi-Comision 11
+ * Leonel Jesus Aballay-Comision 17
+ */
+public interface GenericDAO<T> {
+   
+    void insertar(T entidad) throws Exception;
+    void actualizar(T entidad)throws Exception;
+    void eliminar(int id)throws Exception;
+    T getById(int id)throws Exception;
+    List<T> getAll()throws Exception;
+    
+}
