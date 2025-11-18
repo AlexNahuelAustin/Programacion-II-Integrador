84b93e28bb2ebe3eaf56f1bc292cbca58bdd1555
Alex Austin <austinalex71@gmail.com>
2025-11-18 19:33:17 -0300

    Agregando la capa model

---
+++ HistorialClinico/src/model/Base.java
@@ -0,0 +1,39 @@
+package model;
+
+/**
+ -Grupo: 50
+ -Alumnos: 
+ * Alex Nahuel Austin-Comision 17
+ * Cristian Gabriel Aguirre-Comision 6
+ * Cain Cabrera Bertilazzi-Comision 11
+ * Leonel Jesus Aballay-Comision 17
+ */
+public abstract class Base {
+    private boolean eliminado;
+    int id;
+
+    public Base(boolean eliminado, int id) {
+        this.eliminado = eliminado;
+        this.id = id;
+    }
+
+    public Base() {
+    }
+
+    public boolean isEliminado() {
+        return eliminado;
+    }
+
+    public void setEliminado(boolean eliminado) {
+        this.eliminado = eliminado;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+}
---
+++ HistorialClinico/src/model/Paciente.java
@@ -0,0 +1,83 @@
+package model;
+
+import java.time.LocalDate;
+
+/**
+ -Grupo: 50
+ -Alumnos: 
+ * Alex Nahuel Austin-Comision 17
+ * Cristian Gabriel Aguirre-Comision 6
+ * Cain Cabrera Bertilazzi-Comision 11
+ * Leonel Jesus Aballay-Comision 17
+ */
+public class Paciente extends Base {
+
+    private String nombre;
+    private String apellido;
+    private String dni;
+    private LocalDate fechaNacimiento;
+    private HistorialClinica historial;
+
+    public Paciente(String nombre, String apellido, String dni, LocalDate fechaNacimiento, int id) {
+        super(false, id);
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.fechaNacimiento = fechaNacimiento;
+
+    }
+
+    public Paciente() {
+        super();
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public HistorialClinica getHistorial() {
+        return historial;
+    }
+
+    public void setHistorial(HistorialClinica historial) {
+        this.historial = historial;
+    }
+
+    @Override
+    public String toString() {
+        return "Paciente: \n"
+                + "  id=" + id + "\n"
+                + "  nombre= " + nombre + "\n"
+                + "  apellido= " + apellido + "\n"
+                + "  dni= " + dni + "\n"
+                + "  fechaNacimiento= " + fechaNacimiento;
+    }
+}
